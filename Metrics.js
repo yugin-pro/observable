a22a4234165d6e46059b2cb1f7876ca174f0b20d
Yugin Dimitriy <78262135+yugin-pro@users.noreply.github.com>
2024-08-07 17:43:42 +0300

    Update Metrics.js

--- Metrics.js
+++ Metrics.js
@@ -1,5 +1,28 @@
-export default class Metrics { 
-  constructor(database,...args) {
-    this.db = database
+export default class Metrics extends Map {
+  constructor(database, options = {}) {
+    super()
+    this.database = database
+  }
+  async exe(metric, value = 'value') {
+    let context = this
+    let m =  this.get(metric)
+    let aaa = 'sdf'
+    let exeList = m.match(/(exe\(\w*\))/gm)
+    
+    if (typeof m == "string" && exeList && exeList.length) {
+      for await (let element of exeList) {
+        let value = await context.exe(element.match(/\((\w+)\)/)[1]) 
+        m = m.replace(element,value)
+      }  
+      return eval(m)
+    }
+    
+    if (m instanceof Map) {
+        let [result] = await this.database.query(m.get('query')) 
+        return result[m.get('value')]
+    }
+    
+    let [result] = await this.database.query(m) 
+    return result[value]
   }
 }
